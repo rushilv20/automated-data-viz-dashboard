96b0a1193aae19ef63fba387cd0d8bf96843d801
rushil <rushilvegada@gmail.com>
2024-08-05 12:55:35 -0400

    cors issue

--- backend/controllers/dataController.js
+++ backend/controllers/dataController.js
@@ -4,62 +4,62 @@ const config = require('../constants/config');
 const responseHandler = require('../utils/responseHandler');
 
 const parseDynamoDBData = (data) => {
-    return data.map(item => {
-        const parsedItem = {};
-        for (const key in item) {
-            const valueObj = item[key];
-            if ('S' in valueObj) {
-                parsedItem[key] = valueObj.S;
-            } else if ('N' in valueObj) {
-                parsedItem[key] = parseFloat(valueObj.N);
-            } else if ('BOOL' in valueObj) {
-                parsedItem[key] = valueObj.BOOL;
-            } else if ('NULL' in valueObj) {
-                parsedItem[key] = null;
-            } else if ('M' in valueObj) {
-                parsedItem[key] = parseDynamoDBData([valueObj.M])[0]; // Recursive parse for Map types
-            } else if ('L' in valueObj) {
-                parsedItem[key] = parseDynamoDBData(valueObj.L); // Recursive parse for List types
-            } else {
-                console.error("Unhandled DynamoDB data type:", key, valueObj);
-            }
-        }
-        return parsedItem;
-    });
+  return data.map(item => {
+    const parsedItem = {};
+    for (const key in item) {
+      const valueObj = item[key];
+      if ('S' in valueObj) {
+        parsedItem[key] = valueObj.S;
+      } else if ('N' in valueObj) {
+        parsedItem[key] = parseFloat(valueObj.N);
+      } else if ('BOOL' in valueObj) {
+        parsedItem[key] = valueObj.BOOL;
+      } else if ('NULL' in valueObj) {
+        parsedItem[key] = null;
+      } else if ('M' in valueObj) {
+        parsedItem[key] = parseDynamoDBData([valueObj.M])[0]; // Recursive parse for Map types
+      } else if ('L' in valueObj) {
+        parsedItem[key] = parseDynamoDBData(valueObj.L); // Recursive parse for List types
+      } else {
+        console.error("Unhandled DynamoDB data type:", key, valueObj);
+      }
+    }
+    return parsedItem;
+  });
 };
 
 const getData = async (req, res) => {
-    const tableNames = [config.TRIP_FINANCES, config.LOGGED_FLIGHTS, config.INVOICES];
-    
-    try {
-        const promises = tableNames.map(tableName => {
-            const params = {
-                TableName: tableName,
-            };
-            const command = new ScanCommand(params);
-            return dynamoClient.send(command);
-        });
+  const tableNames = [config.TRIP_FINANCES, config.LOGGED_FLIGHTS, config.INVOICES];
+  
+  try {
+    const promises = tableNames.map(tableName => {
+      const params = {
+        TableName: tableName,
+      };
+      const command = new ScanCommand(params);
+      return dynamoClient.send(command);
+    });
 
-        const results = await Promise.all(promises);
+    const results = await Promise.all(promises);
 
-        const data = {
-            tripFinances: parseDynamoDBData(results[0].Items),
-            loggedFlights: parseDynamoDBData(results[1].Items),
-            invoices: parseDynamoDBData(results[2].Items)
-        };
+    const data = {
+      tripFinances: parseDynamoDBData(results[0].Items),
+      loggedFlights: parseDynamoDBData(results[1].Items),
+      invoices: parseDynamoDBData(results[2].Items)
+    };
 
-        // Set CORS headers explicitly
-        res.setHeader('Access-Control-Allow-Origin', 'https://www.bellairdashboard.com');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    // Set CORS headers explicitly
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.bellairdashboard.com');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-        responseHandler.success(res, data);
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        responseHandler.error(res, error);
-    }
+    responseHandler.success(res, data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    responseHandler.error(res, error);
+  }
 };
 
 module.exports = {
-    getData,
+  getData,
 };
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-// backend/server.js
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -10,15 +9,15 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-    origin: function (origin, callback) {
-        console.log("Origin:", origin); // Debug log to check the origin
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionsSuccessStatus: 200
+  origin: function (origin, callback) {
+    console.log("Origin:", origin); // Debug log to check the origin
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200
 };
 
 app.use(cors(corsOptions));
@@ -31,5 +30,6 @@ app.options('*', cors(corsOptions));
 app.use('/api', dashboardRoutes);
 
 app.listen(3000, () => {
-    console.log('Server running on port 3000');
+  console.log('Server running on port 3000');
 });
+
